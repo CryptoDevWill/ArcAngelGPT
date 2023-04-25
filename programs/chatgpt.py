a8fe1b8d8a7b31f8116f99ec19863d47ded0d13f
Joshua Welch <oneadvent@joshuawelch.me>
2023-04-25 00:22:49 -0700

    initial commit to save python programs

--- programs/chatgpt.py
+++ programs/chatgpt.py
@@ -18,8 +18,19 @@ def ChatGPT(conversation):
             messages=conversation
         )
         response = completion.choices[0].message.content
-        conversation.append({ "role": "assistant", "content": response })
-
+        if response:
+            pattern = r"```python\n(.*?)```"
+            match = re.search(pattern, response, re.DOTALL)
+            if match:
+                code_block = match.group(1)
+                function_pattern = r"def\s+(\w+)\("
+                function_match = re.search(function_pattern, code_block)
+                print(function_match)
+                if function_match:
+                    function_name = function_match.group(1)
+                    file_name = f"{function_name}.py"
+                    save_code_block(code_block, file_name)
+            conversation.append({ "role": "assistant", "content": response })
         result = None
         for command in commands:
             regex = rf'{command} (\S+)'
@@ -32,10 +43,7 @@ def ChatGPT(conversation):
                     result = _touch(argument)
                 break
             else:
-                return response    
-        if result:
-            conversation.append({ "role": "assistant", "content": result })
-            return result
+                return response
     except openai.error.AuthenticationError as e:
         error_message = str(e)
         print(f"Error: {error_message}")
@@ -55,3 +63,15 @@ def _touch(file_name):
         return f"File '{file_name}' has been created successfully."
     else:
         return f"Error creating file '{file_name}': {os.strerror(exit_code)}"
+def save_code_block(code_block, file_name):
+    function_pattern = r"def\s+(\w+)\("
+    function_match = re.search(function_pattern, code_block)
+    if function_match:
+        function_name = function_match.group(1)
+        save_dir = "./user_scripts"
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        file_path = os.path.join(save_dir, file_name)
+        with open(file_path, "w") as f:
+            f.write(code_block)
+            print(f"Code block saved to {file_path}")
