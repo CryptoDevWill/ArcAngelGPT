344d0b8854d4e025f31517411a38325dc85290f9
CryptoDevWill <williamdsanders05@gmail.com>
2023-04-25 01:30:00 -0400

    mkdir and touch

--- programs/chatgpt.py
+++ programs/chatgpt.py
@@ -1,12 +1,14 @@
 # openai_chat.py
 import openai
 import os
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+commands = ["mkdir", "touch"]
 
 # Chat conversation
 def ChatGPT(conversation):
@@ -15,18 +17,40 @@ def ChatGPT(conversation):
             model="gpt-3.5-turbo",
             messages=conversation
         )
-        result = completion.choices[0].message.content
-        conversation.append({ "role": "assistant", "content": result })
+        response = completion.choices[0].message.content
+        conversation.append({ "role": "assistant", "content": response })
 
-    # Scan result for backticks
-        if "```" in result:
-            return "In work mode"
+        result = None
+        for command in commands:
+            regex = rf'{command} (\S+)'
+            match = re.search(regex, response)
+            if match:
+                argument = match.group(1)
+                if command == "mkdir":
+                    result = _mkdir(argument)
+                elif command == "touch":
+                    result = _touch(argument)
+                break
 
-        else:
-            return completion.choices[0].message.content
-    
+        if result:
+            conversation.append({ "role": "assistant", "content": result })
+
+        return response
     except openai.error.AuthenticationError as e:
         error_message = str(e)
         print(f"Error: {error_message}")
         return error_message
-    
+
+def _mkdir(directory_name):
+    exit_code = os.system(f'mkdir {directory_name}')
+    if exit_code == 0:
+        return f"Directory '{directory_name}' has been created successfully."
+    else:
+        return f"Error creating directory '{directory_name}': {os.strerror(exit_code)}"
+
+def _touch(file_name):
+    exit_code = os.system(f"touch {file_name}")
+    if exit_code == 0:
+        return f"File '{file_name}' has been created successfully."
+    else:
+        return f"Error creating file '{file_name}': {os.strerror(exit_code)}"
