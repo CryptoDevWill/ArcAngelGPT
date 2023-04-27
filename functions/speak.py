df93203c8d948edf9518a01f40fd423c3c787386
Joshua Welch <oneadvent@joshuawelch.me>
2023-04-26 19:48:32 -0700

    added button to mute the things voice.

--- functions/speak.py
+++ functions/speak.py
@@ -3,7 +3,12 @@ import tempfile
 from gtts import gTTS
 from playsound import playsound
 
+mute_speech = False
+
 def speak(text):
+    if mute_speech:
+        return
+    
     tts = gTTS(text=text, lang='en')
     
     with tempfile.NamedTemporaryFile(delete=True) as fp:
@@ -11,3 +16,7 @@ def speak(text):
         tts.save(temp_file)
         playsound(temp_file)
         os.remove(temp_file)
+
+def mute_button(mute):
+    global mute_speech
+    mute_speech = mute
--- screens/details_screen.py
+++ screens/details_screen.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 from dotenv import load_dotenv
+from functions.speak import mute_button
 
 load_dotenv()
 
@@ -14,6 +15,13 @@ class DetailsScreen:
         self.api_key_label = tk.Label(self.frame, text=f"OPENAI API KEY: {masked_api_key}")
         self.api_key_label.pack(pady=10)
 
+        # Create a mute button
+        self.mute_button = tk.Button(self.frame, text="Mute Speech", command=self.toggle_mute)
+        self.mute_button.pack(pady=10)
+
+        # Set the initial mute state to False
+        self.mute_speech = False
+
     def mask_api_key(self, api_key):
         if api_key:
             half_length = len(api_key) // 2
@@ -23,6 +31,15 @@ class DetailsScreen:
     def show(self):
         self.frame.lift()
 
+    def toggle_mute(self):
+        self.mute_speech = not self.mute_speech
+        if self.mute_speech:
+            mute_button(True)
+            self.mute_button.config(text="Unmute Speech")
+        else:
+            mute_button(False)
+            self.mute_button.config(text="Mute Speech")
+
 # Create a Tkinter window and display the DetailsScreen
 if __name__ == "__main__":
     root = tk.Tk()
@@ -30,4 +47,5 @@ if __name__ == "__main__":
     root.title('API Key Display')
     details_screen = DetailsScreen(root)
     details_screen.show()
+    details_screen.speak("Hello, world!")
     root.mainloop()
