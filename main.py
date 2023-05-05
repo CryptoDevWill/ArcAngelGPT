412ad2165ba5a8872c0f35a6ad516992cf7d943f
CryptoDevWill <williamdsanders05@gmail.com>
2023-05-05 06:25:30 -0400

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from screens.load_screen import show_loading_screen
 class ArcAngelGPT:
     def __init__(self):
         self.window = tk.Tk()
-        show_loading_screen(self.window)
+        # show_loading_screen(self.window)
         GUI(self.window)
         Menu(self.window)
         self.window.mainloop()
