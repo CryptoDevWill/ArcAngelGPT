2b015e071f7950ae4b41e9ec40c85f75a094a3fc
CryptoDevWill <williamdsanders05@gmail.com>
2023-05-05 06:37:15 -0400

    update

--- functions/play_sound.py
+++ functions/play_sound.py
@@ -1,14 +1,20 @@
 import os
+import sys
 import pygame
+
 
 def play_sound(sound_name, assets_path="assets/"):
     pygame.mixer.init()
 
+    # Get the script's directory and join it with the assets_path
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    assets_dir = os.path.join(script_dir, assets_path)
+
     # Find the sound file in the given directory
     sound_path = None
-    for file in os.listdir(assets_path):
+    for file in os.listdir(assets_dir):
         if file.endswith(".mp3") and file.startswith(sound_name):
-            sound_path = os.path.join(assets_path, file)
+            sound_path = os.path.join(assets_dir, file)
             break
 
     # Play the sound if found
