9b0e3c1a9d505f79bb4bc98ffd444d6b94b9608c
CryptoDevWill <williamdsanders05@gmail.com>
2023-05-06 08:40:04 -0400

    update

--- tools/parse_command.py
+++ tools/parse_command.py
@@ -1,23 +1,20 @@
 import re
-from gui.current_steps import current_tasks_array
-from data.global_variables import thinking
-from data.global_variables import work_mode
-from functions.play_sound import play_sound
-from components.chat.chat_window import ChatWindow
-import subprocess
-from gui.terminal import Terminal
 import os
+import subprocess
+
+from gui.current_steps import current_tasks_array
+from data.global_variables import thinking, work_mode
+from functions.play_sound import play_sound
+from gui.terminal import Terminal
 
 def parse_command(response: str):
     if '```' not in response:
         print("No commands found.")
         return
     work_mode.set(True)
-    chat_window = ChatWindow()  # Instantiate ChatWindow
-    chat_window.update_conversation()
 
-    allowed_commands_unix = ["mkdir", "touch", "echo", "rm", "mv", "cat", "python", "node"]
-    allowed_commands_windows = ["mkdir", "echo", "del", "move", "type", "python", "node"]
+    allowed_commands_unix = ["mkdir", "touch", "echo"]
+    allowed_commands_windows = ["mkdir", "echo"]
     allowed_commands = allowed_commands_unix if os.name == 'posix' else allowed_commands_windows
 
     pattern = r'```(.*?)```'
@@ -35,17 +32,18 @@ def parse_command(response: str):
 
     command_dicts = [{"step": f"Step {i+1}: {cmd}", "command": cmd, "complete": False} for i, cmd in enumerate(commands)]
     current_tasks_array.set(command_dicts)
-    print(current_tasks_array.get())
     execute_command()
 
-
-
-
+def convert_to_cmd_command(command: str):
+    if command.startswith("touch"):
+        file_name = command.split(" ")[1]
+        return f"echo. > {file_name}"
+    else:
+        return command
 
 def execute_command():
-    print("execute command")
     tasks = current_tasks_array.get()
-    terminal_instance = Terminal.instance()  # Get the Terminal instance
+    terminal_instance = Terminal.instance()
     is_posix = os.name == 'posix'
 
     for index, task in enumerate(tasks):
@@ -55,18 +53,16 @@ def execute_command():
             task_string = convert_to_cmd_command(task_string)
 
         try:
-            result = subprocess.run(task_string, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)  # Capture command output
-            output = result.stdout + result.stderr  # Combine stdout and stderr
+            result = subprocess.run(task_string, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            output = result.stdout + result.stderr
         except subprocess.CalledProcessError as e:
-            output = e.stderr  # Print the error message
-        terminal_instance.update_output(output)  # Update the Terminal output
+            output = e.stderr
+        terminal_instance.update_output(output)
         tasks[index]["complete"] = True
-        current_tasks_array.set(tasks)  # Set the current_tasks_array after each iteration
+        current_tasks_array.set(tasks)
         play_sound("task_complete")
-        # time.sleep(1)  # Delay for 3 seconds after each iteration
 
     thinking.set(False)
     work_mode.set(False)
     play_sound("complete")
     current_tasks_array.set([])
-
